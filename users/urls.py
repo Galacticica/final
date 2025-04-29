ef56fef73bd840245a07dc58491405be5070ccd6
Reagan Zierke <reaganzierke@gmail.com>
2025-04-28 22:27:32 -0500

    split views into more readable files

--- users/urls.py
+++ users/urls.py
@@ -8,14 +8,15 @@ Description: URL configuration for the User app.
 
 
 from django.urls import path
-from . import views
-from . import admin_views
+from . import views_user
+from . import views_admin
+from . import views_gamble
 
 urlpatterns = [
-    path('give_money/', admin_views.GiveMoneyView.as_view(), name='give_money'),
-    path('give_xp/', admin_views.GiveXPView.as_view(), name='give_xp'),
-    path('coinflip/', views.CoinFlipBetView.as_view(), name='coinflip_bet'),
-    path('profile/', views.GetProfileView.as_view(), name='profile'),
-    path('delete_user/', admin_views.DeleteUserView.as_view(), name='delete_user'),
-    path('level_up/', views.LevelUpView.as_view(), name='level_up'),
+    path('give_money/', views_admin.GiveMoneyView.as_view(), name='give_money'),
+    path('give_xp/', views_admin.GiveXPView.as_view(), name='give_xp'),
+    path('coinflip/', views_gamble.CoinFlipBetView.as_view(), name='coinflip_bet'),
+    path('profile/', views_user.GetProfileView.as_view(), name='profile'),
+    path('delete_user/', views_admin.DeleteUserView.as_view(), name='delete_user'),
+    path('level_up/', views_user.LevelUpView.as_view(), name='level_up'),
 ]
--- users/views_user.py
+++ users/views_user.py
@@ -3,7 +3,7 @@ File: views.py
 Author: Reagan Zierke
 Date: 2025-04-27
 Description: Views for the Users app.
-This file contains the views for non-admin user-related operations such as giving money, coin flip betting, etc.
+This file contains the views for non-admin user-related operations such as viewing profile, level up, etc.
 """
 
 from rest_framework.views import APIView
@@ -11,7 +11,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from . import serializers as cereal
 from .models import CustomUser
-import random
 
 class GetProfileView(APIView):
     '''
@@ -42,40 +41,6 @@ class GetProfileView(APIView):
 
         serializer = cereal.CustomUserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK if not created else status.HTTP_201_CREATED)
-
-
-class CoinFlipBetView(APIView):
-    '''
-    View to place a bet on a coin flip.
-    This view requires a discord_id, bet amount, and side (heads or tails) in the request data.
-    It checks if the user has enough money to place the bet and updates their balance accordingly.
-    '''
-
-    def post(self, request):
-        discord_id = request.data.get('discord_id')
-
-        if not discord_id:
-            return Response({"error": "discord_id is required"}, status=status.HTTP_400_BAD_REQUEST)
-
-        serializer = cereal.CoinFlipBetSerializer(data=request.data)
-        if serializer.is_valid():
-            bet = serializer.data.get('bet')
-            side = serializer.data.get('side')
-
-            result = random.choice(["heads", "tails"])
-            if result == side.lower():
-                serializer.user.money += bet  
-                message = f"You won! The coin landed on {result}. Your new balance is {serializer.user.money}."
-            else:
-                serializer.user.money -= bet 
-                message = f"You lost! The coin landed on {result}. Your new balance is {serializer.user.money}."
-
-            serializer.user.save()
-
-            return Response({"message": message, "balance": serializer.user.money}, status=status.HTTP_200_OK)
-
-        else:
-            return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 class LevelUpView(APIView):
     '''
