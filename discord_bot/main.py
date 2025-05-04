83a79a09d44faec5a4315674fa4be47713d0ef0c
Reagan Zierke <reaganzierke@gmail.com>
2025-05-04 12:57:37 -0500

    minor changes

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -32,7 +32,7 @@ async def on_ready():
     print("Setting bot presence...")
     await bot.change_presence(
         status=discord.Status.online,
-        activity=discord.Activity(type=discord.ActivityType.watching, name="Painfully Existing")
+        activity=discord.Activity(type=discord.ActivityType.listening, name="The Abyss Of Space")
     )
     print('Bot is online!')
 
